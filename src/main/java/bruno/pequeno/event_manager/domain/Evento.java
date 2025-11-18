b5a103e7e12cd9bb170daa9d11c44714a3d30c5f
Bruno Pequeno <99445630+bruno-pequeno@users.noreply.github.com>
2025-11-18 15:42:49 +0000

    feat: :zap: Validando a criação da inscrição de um usuário e fluxo de fila

--- src/main/java/bruno/pequeno/event_manager/domain/Evento.java
+++ src/main/java/bruno/pequeno/event_manager/domain/Evento.java
@@ -1,6 +1,9 @@
 package bruno.pequeno.event_manager.domain;
 
+import bruno.pequeno.event_manager.domain.enums.StatusInscricao;
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -11,6 +14,7 @@ public class Evento {
     private String local;
     private LocalDateTime dataEvento;
     private Map<String, TipoIngresso> ingressos;
+    private final List<Inscricao> inscricoes = new ArrayList<>();
 
     public Evento(String nome, String local, LocalDateTime dataEvento, List<TipoIngresso> ingressos) {
         validarNome(nome);
@@ -75,6 +79,38 @@ public class Evento {
         if (dataEvento == null || dataEvento.isBefore(LocalDateTime.now())) {
             throw new IllegalArgumentException("Data do evento não pode ser null e nem no passado");
         }
+    }
+
+    public Inscricao inscreverUsuario(Long usuarioId, String nomeTipo) {
+        TipoIngresso tipoIngresso = buscarTipoIngresso(nomeTipo);
+
+        if (usuarioJaInscrito(usuarioId)) {
+            throw new IllegalArgumentException("Usuário já está inscrito e confirmado nesse evento!");
+        }
+
+        if (!temVagaDisponivel(tipoIngresso)) {
+            throw new IllegalArgumentException("Não há mais vagas para esse evento!");
+        }
+
+        Inscricao inscricao = new Inscricao(usuarioId, tipoIngresso);
+        inscricoes.add(inscricao);
+
+        return inscricao;
+    }
+
+    private boolean usuarioJaInscrito(Long usuarioId) {
+        return inscricoes.stream()
+                .anyMatch(inscricao -> inscricao.getIdUsuario().equals(usuarioId)
+                        && inscricao.getStatusInscricao() != StatusInscricao.CANCELADA);
+    }
+
+    private boolean temVagaDisponivel(TipoIngresso tipo) {
+        long inscricoesAtivas = inscricoes.stream()
+                .filter(i -> i.getTipoIngresso().equals(tipo))
+                .filter(i -> i.getStatusInscricao() != StatusInscricao.CANCELADA)
+                .count();
+
+        return inscricoesAtivas < tipo.getLimiteVagas();
     }
 
     public Long getId() {
--- src/test/java/bruno/pequeno/event_manager/domain/EventoTest.java
+++ src/test/java/bruno/pequeno/event_manager/domain/EventoTest.java
@@ -1,5 +1,6 @@
 package bruno.pequeno.event_manager.domain;
 
+import bruno.pequeno.event_manager.domain.enums.StatusInscricao;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -57,5 +58,79 @@ public class EventoTest {
         List<TipoIngresso> ingressos = new ArrayList<>();
 
         assertThrows(IllegalArgumentException.class, () -> new Evento(nome, local, dataEvento, ingressos));
+    }
+
+    @Test
+    void deveInscreverUsuarioEmEvento() {
+        String nome = "JavaConf 2025";
+        String local = "São Paulo";
+        LocalDateTime data = LocalDateTime.now().plusDays(30);
+
+        TipoIngresso vip = new TipoIngresso("VIP", 20, BigDecimal.valueOf(220));
+        List<TipoIngresso> tipos = List.of(vip);
+
+        Evento evento = new Evento(nome, local, data, tipos);
+        Long usuarioId = 1L;
+
+        Inscricao inscricao = evento.inscreverUsuario(usuarioId, "VIP");
+
+        assertNotNull(inscricao);
+        assertEquals(usuarioId, inscricao.getIdUsuario());
+        assertEquals(vip, inscricao.getTipoIngresso());
+        assertEquals(StatusInscricao.PENDENTE, inscricao.getStatusInscricao());
+    }
+
+    @Test
+    void naoDeveInscreverUsuarioSemVagasDisponiveis() {
+        String nome = "JavaConf 2025";
+        String local = "São Paulo";
+        LocalDateTime data = LocalDateTime.now().plusDays(30);
+
+        TipoIngresso vip = new TipoIngresso("VIP", 2, BigDecimal.valueOf(220));
+        List<TipoIngresso> tipos = List.of(vip);
+
+        Evento evento = new Evento(nome, local, data, tipos);
+
+        evento.inscreverUsuario(1L, "VIP");
+        evento.inscreverUsuario(2L, "VIP");
+
+        assertThrows(IllegalArgumentException.class, () -> evento.inscreverUsuario(3L, "VIP"));
+    }
+
+    @Test
+    void naoDeveInscreverMesmoUsuarioDuasVezes() {
+        String nome = "JavaConf 2025";
+        String local = "São Paulo";
+        LocalDateTime data = LocalDateTime.now().plusDays(30);
+
+        TipoIngresso vip = new TipoIngresso("VIP", 20, BigDecimal.valueOf(220));
+        List<TipoIngresso> tipos = List.of(vip);
+
+        Evento evento = new Evento(nome, local, data, tipos);
+
+        evento.inscreverUsuario(1L, "VIP");
+
+        assertThrows(IllegalArgumentException.class, () -> evento.inscreverUsuario(1L, "VIP"));
+    }
+
+    @Test
+    void deveLiberarVagaAoCancelarInscricao() {
+        String nome = "JavaConf 2025";
+        String local = "São Paulo";
+        LocalDateTime data = LocalDateTime.now().plusDays(30);
+
+        TipoIngresso vip = new TipoIngresso("VIP", 2, BigDecimal.valueOf(220)); // Só 2 vagas
+        List<TipoIngresso> tipos = List.of(vip);
+
+        Evento evento = new Evento(nome, local, data, tipos);
+
+        Inscricao inscricaoQueSeraCancelada = evento.inscreverUsuario(1L, "VIP");
+        evento.inscreverUsuario(2L, "VIP");
+
+        assertThrows(IllegalArgumentException.class, () -> evento.inscreverUsuario(3L, "VIP"));
+
+        inscricaoQueSeraCancelada.cancelar();
+
+        assertDoesNotThrow(() -> evento.inscreverUsuario(3L, "VIP"));
     }
 }
